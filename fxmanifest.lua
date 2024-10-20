afcf1d32da533cc57100f7252c6cf529c82f5feb
KingzzGamingg <83891867+kingofkingzcd@users.noreply.github.com>
2024-10-20 11:59:10 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'KingzzGamingg Development XMLs'
+
+dependency 'objectloader'
+file 'lakayhomestead.xml'
+objectloader_map 'lakayhomestead.xml'
